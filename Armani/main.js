0ebcef3d332451c119a517580164151de9da68b7
Hoohiyoo <130562879+Hoohiyoo@users.noreply.github.com>
2023-04-28 02:52:59 +0700

    repair armani

--- Armani/main.js
+++ Armani/main.js
@@ -4,49 +4,51 @@ const grid = document.querySelector('.grid')
 const gridItems = [...grid.querySelectorAll('.grid-img')]
 
 preloadImages('.img').then( _ => {
+
+    const body = document.body
+    body.classList.remove('loading')
 	// Smooth scrolling initialization (using Lenis https://github.com/studio-freight/lenis)
-document.body.classList.remove('loading')
-const lenis = new Lenis({
-    lerp: 0.1,
-    smooth: true
-})
+    const lenis = new Lenis({
+        lerp: 0.1,
+        smooth: true
+    })
 
-const scrollFn = () => {
-    lenis.raf()
+    const scrollFn = () => {
+        lenis.raf()
+        requestAnimationFrame(scrollFn)
+    }
+
     requestAnimationFrame(scrollFn)
-}
 
-requestAnimationFrame(scrollFn)
+    let winsize;
+    const calcWindowSize = () => {
+        winsize = {width: window.innerWidth, height: window.innerHeight}
+    }
+    calcWindowSize()
+    window.addEventListener('resize', calcWindowSize)
 
-let winsize;
-const calcWindowSize = () => {
-    winsize = {width: window.innerWidth, height: window.innerHeight}
-}
-calcWindowSize()
-window.addEventListener('resize', calcWindowSize)
+    gridItems.forEach(item => {
+        
+        gsap.timeline()
+        .set(grid, {
+            perspective: 1000,
+            perspectiveOrigin: `50% ${winsize.height/2 + lenis.scroll}px`
+        })
+        .to(item, {
+            ease: 'none',
+            startAt: {rotationX: 70, scale: 0.7},
+            scale: 1,
+            rotationX: -70,
+            scrollTrigger: {
+                trigger: item,
+                start: 'top bottom',
+                end: 'bottom top',
+                scrub: true,
+                onUpdate: self => gsap.set(grid, {
+                    perspectiveOrigin: () => `50% ${winsize.height/2 + lenis.scroll}px`
+                })
+            }
+        })
 
-gridItems.forEach(item => {
-    
-    gsap.timeline()
-    .set(grid, {
-        perspective: 1000,
-        perspectiveOrigin: `50% ${winsize.height/2 + lenis.scroll}px`
     })
-    .to(item, {
-        ease: 'none',
-        startAt: {rotationX: 70, scale: 0.7},
-        scale: 1,
-        rotationX: -70,
-        scrollTrigger: {
-            trigger: item,
-            start: 'top bottom',
-            end: 'bottom top',
-            scrub: true,
-            onUpdate: self => gsap.set(grid, {
-                perspectiveOrigin: () => `50% ${winsize.height/2 + lenis.scroll}px`
-            })
-        }
-    })
-
-})
 })
