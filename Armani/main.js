1cf1c26e1b51358618a715787b0738f45784c516
Hoohiyoo <130562879+Hoohiyoo@users.noreply.github.com>
2023-04-28 19:35:12 +0700

    rep

--- Armani/main.js
+++ Armani/main.js
@@ -1,61 +1,59 @@
-const content = document.querySelector('main')
-const images = [...content.querySelectorAll('.img')];
-const promises = [];
-
-images.forEach((image) => {
-  const promise = new Promise((resolve) => {
-    image.addEventListener('load', resolve);
-  });
-  promises.push(promise);
-});
-
 const grid = document.querySelector('.grid')
 const gridItems = [...grid.querySelectorAll('.grid-img')]
 
-Promise.all(promises).then(() =>{
+const extradelay = 3000
+const imgLoad = imagesLoaded('.img')
+
+imgLoad.on('done', instance =>{
 	// Smooth scrolling initialization (using Lenis https://github.com/studio-freight/lenis)
-document.body.classList.remove('loading')
-const lenis = new Lenis({
-    lerp: 0.1,
-    smooth: true
-})
-
-const scrollFn = () => {
-    lenis.raf()
-    requestAnimationFrame(scrollFn)
-}
-
-requestAnimationFrame(scrollFn)
-
-let winsize;
-const calcWindowSize = () => {
-    winsize = {width: window.innerWidth, height: window.innerHeight}
-}
-calcWindowSize()
-window.addEventListener('resize', calcWindowSize)
-
-gridItems.forEach(item => {
+    gsap.to('.loading',{
+        duration: 0.6,
+        ease: 'power2.inOut',
+        delay: extradelay / 1000,
+        y: '-100%'
+    })
     
-    gsap.timeline()
-    .set(grid, {
-        perspective: 1000,
-        perspectiveOrigin: `50% ${winsize.height/2 + lenis.scroll}px`
-    })
-    .to(item, {
-        ease: 'none',
-        startAt: {rotationX: 70, scale: 0.7},
-        scale: 1,
-        rotationX: -70,
-        scrollTrigger: {
-            trigger: item,
-            start: 'top bottom',
-            end: 'bottom top',
-            scrub: true,
-            onUpdate: self => gsap.set(grid, {
-                perspectiveOrigin: () => `50% ${winsize.height/2 + lenis.scroll}px`
-            })
-        }
+    const lenis = new Lenis({
+        lerp: 0.1,
+        smooth: true
     })
 
-})
+    const scrollFn = () => {
+        lenis.raf()
+        requestAnimationFrame(scrollFn)
+    }
+
+    requestAnimationFrame(scrollFn)
+
+    let winsize;
+    const calcWindowSize = () => {
+        winsize = {width: window.innerWidth, height: window.innerHeight}
+    }
+    calcWindowSize()
+    window.addEventListener('resize', calcWindowSize)
+
+    gridItems.forEach(item => {
+        
+        gsap.timeline()
+        .set(grid, {
+            perspective: 1000,
+            perspectiveOrigin: `50% ${winsize.height/2 + lenis.scroll}px`
+        })
+        .to(item, {
+            ease: 'none',
+            startAt: {rotationX: 70, scale: 0.7},
+            scale: 1,
+            rotationX: -70,
+            scrollTrigger: {
+                trigger: item,
+                start: 'top bottom',
+                end: 'bottom top',
+                scrub: true,
+                onUpdate: self => gsap.set(grid, {
+                    perspectiveOrigin: () => `50% ${winsize.height/2 + lenis.scroll}px`
+                })
+            }
+        })
+
+    })
 })
